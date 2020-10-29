bc3ea0976b7babe8b259853695d1d2c5abfa9c4d
Kevin van Helden <kevin.van.helden@glamorousgoat.nl>
2020-10-29 20:02:15 +0100

    added searchbar to header

--- src/components/views/Header/Header.tsx
+++ src/components/views/Header/Header.tsx
@@ -1,7 +1,7 @@
 import React, { Fragment, useState } from "react";
 import styles from "./Header.module.scss";
 import classnames from "classnames";
-import { Text } from '../../elements';
+import { Searchbar, Text } from '../../elements';
 import logo from '../../../images/logo.svg';
 import hamburger from '../../../images/icons/hamburger.svg';
 import search from '../../../images/icons/search.svg';
@@ -19,7 +19,7 @@ export default function Header() {
           <img className={classnames(styles.logo)} src={logo} alt={"logo"} />
         </a>
         <div className={classnames(styles.actions)}>
-          <img onClick={() => openSearch(true)} className={classnames(styles.search)} src={search} alt={"search"} />
+          <img onClick={() => openSearch(!activeSearch)} className={classnames(styles.search)} src={search} alt={"search"} />
           <img onClick={() => openMenu(true)} className={classnames(styles.hamburger)} src={hamburger} alt={"hamburger"} />
         </div>
       </header>
@@ -34,7 +34,7 @@ export default function Header() {
           </div>
           <div className={classnames(styles.content)}>
             <Text text={"Categories"} variant={"h1"} />
-            <Text text={"All clothing"} link={'/all'} />
+            <Text text={"All clothing"} link={'/all-clothing'} />
           </div>
         </div>
       </div>
@@ -47,6 +47,7 @@ export default function Header() {
         </div>
         <div className={classnames(styles.content)}>
           <Text text={"Search River Island"} variant={"h1"} />
+          <Searchbar />
         </div>
       </div>
     </Fragment>
