2af616abf048477cfe20f3240958a3b7ea359352
Kevin van Helden <kevin.van.helden@glamorousgoat.nl>
2020-10-29 20:57:11 +0100

    searchbar external reference & onchangefunc added

--- src/components/elements/Searchbar/Searchbar.tsx
+++ src/components/elements/Searchbar/Searchbar.tsx
@@ -3,33 +3,42 @@ import styles from "./Searchbar.module.scss";
 import classnames from "classnames";
 import close from '../../../images/icons/x.svg';
 
-export default function Searchbar() {
+type Props = {
+  reference: any,
+  onchangeFunc?: () => void,
+}
+
+export default function Searchbar(props: Props) {
   const [activeIcon, toggleIcon] = useState(false);
   const [inputValue, setInputValue] = useState('');
-  const inputEl = useRef<HTMLInputElement>(null);
   const imageEl = useRef<HTMLImageElement>(null);
 
   useEffect(() => {
-    if (inputEl && inputEl.current) inputEl.current.addEventListener("input", handleChangeEvents);
+    const { reference } = props;
+    if (reference && reference.current) reference.current.addEventListener("input", handleChangeEvents);
     if (imageEl && imageEl.current) imageEl.current.addEventListener("click", deleteInputValue);
   });
 
   // Update value in state and check if delete icon should disappear
   const handleChangeEvents = () => {
+    const { onchangeFunc } = props;
     changeValue();
     checkDeleteIcon();
+    onchangeFunc && onchangeFunc();
   }
 
   // Update input value on input change
   const changeValue = () => {
-    if (inputEl && inputEl.current) {
-      setInputValue(inputEl.current.value)
+    const { reference } = props;
+    if (reference && reference.current) {
+      setInputValue(reference.current.value)
     }
   }
 
   // Check if the delete icon should disappear
   const checkDeleteIcon = () => {
-    if (inputEl && inputEl.current && inputEl.current.value.length !== 0) {
+    const { reference } = props;
+    if (reference && reference.current && reference.current.value.length !== 0) {
       toggleIcon(true)
     } else {
       toggleIcon(false)
@@ -38,17 +47,22 @@ export default function Searchbar() {
 
   // Delete icon on click event
   const deleteInputValue = () => {
+    const { onchangeFunc } = props;
+    const { reference } = props;
     // Change input value to nothing
-    if (inputEl && inputEl.current) {
+    if (reference && reference.current) {
       setInputValue('')
     };
     // Check if the delete icon should disappear
     checkDeleteIcon();
+    onchangeFunc && onchangeFunc();
   }
+
+  const { reference } = props;
 
   return (
     <div className={classnames(styles.root)}>
-      <input ref={inputEl} value={inputValue} className={classnames(styles.input)} placeholder={"Search for clothing"} />
+      <input ref={reference} value={inputValue} className={classnames(styles.input)} placeholder={"Search for clothing"} />
       <img ref={imageEl} className={classnames(styles.closeIcon, { [styles.active]: activeIcon })} src={close} alt={"close-icon"} />
     </div>
   );
