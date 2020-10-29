4619dff231aae77e46fcd8cb2fd22b17e8bda5cc
Kevin van Helden <kevin.van.helden@glamorousgoat.nl>
2020-10-29 16:52:47 +0100

    Text component

---
+++ src/components/elements/Text/Text.tsx
@@ -0,0 +1,47 @@
+import React, { Fragment } from "react";
+import styles from "./Text.module.scss";
+import classnames from "classnames";
+
+type Props = {
+  text: string,
+  variant?: any,
+  link?: string,
+  strong?: boolean,
+  clickFunc?: () => void,
+}
+
+function Text(props: Props) {
+  const { text, variant = "p", strong = false, link, clickFunc } = props;
+  let ComponentType = variant;
+
+  return (
+    <Fragment>
+      {link ? (
+        <a className={classnames(styles.link)} href={link}>
+          <ComponentType
+            className={classnames(
+              styles.root, styles.link,
+              { [styles.strong]: strong },
+            )}
+            onClick={clickFunc}
+          >
+            {text}
+          </ComponentType>
+        </a>
+      ) : (
+          <ComponentType
+            className={classnames(
+              styles.root,
+              { [styles.strong]: strong },
+            )}
+            onClick={clickFunc}
+          >
+            {text}
+          </ComponentType>
+        )}
+    </Fragment>
+  );
+}
+
+
+export default Text;
