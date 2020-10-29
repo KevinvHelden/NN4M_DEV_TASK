efc81cc776f1d68bf6c8667ee6a0cfa0db066b22
Kevin van Helden <kevin.van.helden@glamorousgoat.nl>
2020-10-29 16:52:38 +0100

    made component seperated folders

---
+++ src/components/elements/index.js
@@ -0,0 +1,5 @@
+/**
+ * Elements Components
+ */
+
+export { default as Text } from "./Text/Text";
---
+++ src/components/views/index.js
@@ -0,0 +1,5 @@
+/**
+ * View Components
+ */
+
+export { default as Header } from "./Header/Header";
