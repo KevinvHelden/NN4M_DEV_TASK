c9d12f641bc95b2c6b3f066ece889bd09a955dac
Kevin van Helden <kevin.van.helden@glamorousgoat.nl>
2020-10-29 22:22:29 +0100

    search page can read keyword param from url

--- src/routes/Search/Search.tsx
+++ src/routes/Search/Search.tsx
@@ -4,12 +4,20 @@ import classnames from "classnames";
 import { Text } from '../../components/elements';
 
 export default function Search() {
-    return (
 
+    const getKeyword = () => {
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+        const keyword = urlParams.get('keyword');
+        return keyword;
+    }
+
+    return (
         <main className={classnames(styles.root)}>
-            <div className={classnames(styles.greeting)}>
-                <Text text={"Welcome to River Island"} variant={"h1"} />
-                <Text text={"Open the menu to get started."} />
+            <div className={classnames(styles.summary)}>
+                <Text text={`Search results for: `} variant={"h1"} />
+                <Text text={`"${getKeyword()}"`} variant={"h1"} />
+                <Text text={"215 products"} />
             </div>
         </main>
     )
