0dce4dda5e40da4e419c8ac86965e6f1e512bfc0
Kevin van Helden <kevin.van.helden@glamorousgoat.nl>
2020-10-30 18:51:34 +0100

    added fetch func to get current product

--- src/routes/Product/Product.tsx
+++ src/routes/Product/Product.tsx
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./Product.module.scss";
 import classnames from "classnames";
 import { Text } from '../../components/elements';
+import { fetch } from '../../helpers';
 
 export default function Product() {
+    const [product, setProduct] = useState({
+        prodid: "",
+        name: "",
+        cost: ""
+    });
+
+    useEffect(() => {
+        fetch.product(getProductName(), setProduct);
+    }, []);
+
+    const getProductName = () => {
+        // All of the url parameters
+        const queryString = window.location.search;
+        // Uses the url to search for the id parameter
+        const productName = new URLSearchParams(queryString).get('name');
+        // Returns the id from the url
+        return productName;
+    }
+
     return (
         <main className={classnames(styles.root)}>
             <div className={classnames(styles.bannerContainer)}>
-                <img src={""} alt={""} />
+                <img src={`http://riverisland.scene7.com/is/image/RiverIsland/${product.prodid}_main`} alt={product.name} />
             </div>
             <div className={classnames(styles.description)}>
-                <Text text={"Product title"} variant={"h1"} />
-                <Text text={"Product price"} />
+                <Text text={product.name} variant={"h1"} />
+                <Text text={`£${product.cost}`} />
             </div>
         </main>
     )
